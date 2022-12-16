5322dfb91d21647b28ac8ee8609adf6e0451b6a5
DreamChaserDeekay <deekayshin@gmail.com>
2022-12-16 14:47:48 +0900

    first commit

---
+++ src/main/java/behomeworkpreprojectgithubtest/BeHomeworkPreProjectGithubTestApplication.java
@@ -0,0 +1,13 @@
+package behomeworkpreprojectgithubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeHomeworkPreProjectGithubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeHomeworkPreProjectGithubTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/behomeworkpreprojectgithubtest/BeHomeworkPreProjectGithubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package behomeworkpreprojectgithubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeHomeworkPreProjectGithubTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
